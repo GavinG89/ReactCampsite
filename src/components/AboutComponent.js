2bccf52cd42bf82a87082b0368c2947ef6773568
Gavin Gray <gavinasaurus@gmail.com>
2021-04-22 18:48:32 -0500

    forms

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { unstable_concurrentAct } from 'react-dom/test-utils';
 
+
+const RenderPartner = ({partner}) => {
+    if(partner) {
+    return(
+        <>
+            <Media object src={partner.image} alt={partner.name} width="150" />
+            <Media body className="ml-5 mb-4">
+                <Media heading>
+                    {partner.name}
+                </Media>
+                {partner.description}
+            </Media>
+        </>
+    );
+    }
+    return(
+        <div />
+    );
+}
 
 function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner} />
+            </Media>
         );
     });
 
